4eb3b45f554f8ed20e68f2789a93d8681c4701e7
Garett Hicks <grhicks@comcast.net>
2020-02-06 16:34:13 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your first and last name: ");
+
+            
+            string input = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+            Print_Name(input);
+        } 
+
+        
+        private static void Print_Name(string input)
+        {
+            Console.WriteLine("Hello " + input + "!");
+        }
+    }
+}
